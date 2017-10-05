410d8222cd02b6d6d5adf5a9876d11c1e7d05437
johnnymoctezuma <johnnymocc@gmail.com>
2017-10-04 19:48:03 -0700

    code is sequelized

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var db = require("./models");
 
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "./public"));
-
+app.use(express.static("public"));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 // override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({
-  defaultLayout: "main"
+    defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
 
@@ -27,4 +27,9 @@ app.use("/create", routes);
 
 // listen on port 3000
 var port = process.env.PORT || 3000;
-app.listen(port);
+
+db.sequelize.sync().then(function() {
+    app.listen(port, function() {
+        console.log("listening....", port)
+    });
+});
